9fb812429ed027e23634f7fb7a5e79fe0f3262dc
Creepermania9 <creepermania9@live.com>
2016-02-12 17:11:42 -0500

    added gamepad controls

--- src/org/usfirst/frc/team2554/robot/Robot.java
+++ src/org/usfirst/frc/team2554/robot/Robot.java
@@ -7,15 +7,16 @@ import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.*;
-
+import edu.wpi.first.wpilibj.Talon;
 
 public class Robot extends SampleRobot {
     RobotDrive myRobot;  // class that handles basic drive operations
-    Joystick rightStick; // set to ID 1 in DriverStation
+    Joystick rightStick, controller; // set to ID 1 in DriverStation
     double right;
     SendableChooser autoChooser;
     DefaultAutonomous defaultAutonomous;
     Command autonomousCommand, rtAuto;
+    Talon armLeft,armRight;
     public void initSmartBoard()
     {
     	autoChooser.addDefault("Under Low Bar", defaultAutonomous);
@@ -23,9 +24,12 @@ public class Robot extends SampleRobot {
         SmartDashboard.putData("Auto Command",autoChooser);
     }
      public Robot() {
-        myRobot = new RobotDrive(0, 1,2,3);
+        myRobot = new RobotDrive(0,1,2,3);
+        armLeft = new Talon(4);
+        armRight = new Talon(5);
         myRobot.setExpiration(0.1);
         rightStick = new Joystick(1);
+        controller = new Joystick(2);
         autoChooser = new SendableChooser();
         defaultAutonomous = new DefaultAutonomous(myRobot);
         rtAuto = new rtAutonomous();
@@ -44,13 +48,9 @@ public class Robot extends SampleRobot {
         myRobot.setSafetyEnabled(true);
         while (isOperatorControl() && isEnabled()) 
         {
-        	/*
-        	Might not need:
-        	if(rightStick.getY()>0)
-        		right = -rightStick.getMagnitude();
-        	if(rightStick.getY()<0)
-        		right = rightStick.getMagnitude();*/
         	myRobot.arcadeDrive(rightStick, 2,rightStick,1);
+        	armLeft.set(controller.getRawAxis(1)/5.0);
+        	armRight.set(controller.getRawAxis(5)/5.0);
             Timer.delay(0.005);		// wait for a motor update time
         }
     }
