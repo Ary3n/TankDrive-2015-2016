443196c744d18e3e533bda7ab727c6f48cec1cfe
Kevin Yang <creepermania9@live.com>
2016-10-27 15:43:35 -0400

    Finish base of AutoAim code
    Should work

--- src/org/usfirst/frc/team2554/robot/AutoAim.java
+++ src/org/usfirst/frc/team2554/robot/AutoAim.java
@@ -1,5 +1,33 @@
 package org.usfirst.frc.team2554.robot;
 
-public class AutoAim {
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
+public class AutoAim {
+	NetworkTable server = NetworkTable.getTable("SmartDashboard");
+	double xValue, yValue;
+	final double DEADZONE = 30;
+	
+	public void run(RobotDrive myRobot, Victor shooterArm, RobotDrive shooter)
+	{
+		changeValues();
+		if(xValue > DEADZONE)
+				myRobot.arcadeDrive(0,-0.1);
+		if(xValue < -DEADZONE)
+				myRobot.arcadeDrive(0,0.1);
+		if(yValue < DEADZONE)
+				shooterArm.set(0.1);
+		if(yValue > DEADZONE * 2)
+				shooterArm.set(-0.1);
+		
+		if(xValue < DEADZONE && xValue > -DEADZONE && yValue > DEADZONE && yValue < 2*DEADZONE)
+			shooter.arcadeDrive(0,0.6);
+	}
+	private void changeValues()
+	{
+		xValue = server.getNumber("magnitudeX", 0.0);
+		yValue = server.getNumber("magnitudeY", 0.0);
+			
+	}
 }
